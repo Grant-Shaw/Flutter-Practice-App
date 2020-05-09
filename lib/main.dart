514add056b5241cf7082aa0f5c597c3b9d76aea4
Grant Shaw <40340717@live.napier.ac.uk>
2020-05-09 07:58:14 +0100

    Font weight changes and new text boxes

--- lib/main.dart
+++ lib/main.dart
@@ -23,17 +23,38 @@ class IdCard extends StatelessWidget{
             Text('Name',
             style: TextStyle(
               color: Colors.grey[400],
-              fontSize: 35,
+              fontSize: 15,
             )
             ),
+            SizedBox(height: 10.0),
             Text(
               'Grant',
               style: TextStyle(
                 color: Colors.amberAccent,
+                fontWeight: FontWeight.bold,
                 letterSpacing: 2.0,
-                fontSize: 25,
+                fontSize: 20,
               ),
+            ),
+            SizedBox(height: 30),
+            Text('Current Age',
+            style: TextStyle(
+              color: Colors.grey[400],
+              fontSize: 15,
             )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '27, I think',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 30.0),
+
           ],
           )
            
