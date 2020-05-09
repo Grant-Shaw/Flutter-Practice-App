ee41116b5d07468fd3b17b1398938f3e36b13b8f
Grant Shaw <40340717@live.napier.ac.uk>
2020-05-09 07:52:03 +0100

    "Basic styling for page"

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,36 @@ class IdCard extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.red[600],
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
         title: Text('My ID Card'),
-
-      )
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Name',
+            style: TextStyle(
+              color: Colors.grey[400],
+              fontSize: 35,
+            )
+            ),
+            Text(
+              'Grant',
+              style: TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 25,
+              ),
+            )
+          ],
+          )
+           
+        )
     
     );
   }
