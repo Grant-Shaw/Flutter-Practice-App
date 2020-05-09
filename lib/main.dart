a7edd5074812a11c2a762ee28174668a53ad38fe
Grant Shaw <40340717@live.napier.ac.uk>
2020-05-09 07:19:58 +0100

    More Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  
+void main() => runApp(MaterialApp(
+  home: IdCard(),
+));
+
+class IdCard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.red[600],
+      appBar: AppBar(
+        title: Text('My ID Card'),
+
+      )
+    
+    );
+  }
+
+
+
+
+
 }
+
+
+  
+
